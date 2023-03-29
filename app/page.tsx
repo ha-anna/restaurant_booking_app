cd174c73ad0bfbd41afedeff4e84c7f3ac4746ff
Ha Anna <its.haanna@gmail.com>
2023-03-29 21:18:13 +0900

    Upload: adding components

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,16 @@
 "use client"
 import Link from 'next/link'
-import { useRouter } from 'next/navigation';
-import * as React from 'react';
-import { useState } from 'react';
+import NavBar from './components/NavBar'
+import Header from './components/Header'
 
 
 export default function Home() {
-  const router = useRouter()
-  const [location, setLocation] = useState("")
+
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
-        {/* NAVBAR */}
-        <nav className="bg-white p-2 flex justify-between">
-        <Link href="/" className="font-bold text-gray-700 text-2xl">
-          OpenTable
-        </Link>
-          <div>
-            <div className="flex">
-              <button
-                className="bg-blue-400 text-white border p-1 px-4 rounded mr-3"
-              >
-                Sign in
-              </button>
-              <button className="border p-1 px-4 rounded">Sign up</button>
-            </div>
-          </div>
-        </nav>
-        {/* NAVBAR */}
-        <main>
-          {/* HEADER */}
-          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
-            <div className="text-center mt-10">
-              <h1 className="text-white text-5xl font-bold mb-2">
-                Find your table for any occasion
-              </h1>
-              {/* SEARCH BAR */}
-              <div className="text-left text-lg py-3 m-auto flex justify-center">
-                <input
-                  className="rounded  mr-3 p-2 w-[450px]"
-                  type="text"
-                  placeholder="State, city or town"
-                  value={location}
-                  onChange={(e) => setLocation(e.target.value)}
-                />
-                <button className="rounded bg-red-600 px-9 py-2 text-white" onClick={() => {
-                  if(location === "banana") return
-                  router.push("/search")
-                }}>
-                  Let's go
-                </button>
-              </div>
-              {/* SEARCH BAR */}
-            </div>
-          </div>
-          {/* HEADER */} {/* CARDS */}
+        <NavBar />
+        <Header />
+       {/* CARDS */}
           <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
             {/* CARD */}
             <div
@@ -84,8 +40,7 @@ export default function Home() {
             {/* CARD */}
           </div>
           {/* CARDS */}
-        </main>
+        </div>
       </main>
-    </main>
   )
 }
