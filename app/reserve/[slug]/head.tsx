a0570998233deed55e09634cc61752e905808921
Ha Anna <its.haanna@gmail.com>
2023-04-01 23:25:00 +0900

    Upload: code formatting

--- app/reserve/[slug]/head.tsx
+++ app/reserve/[slug]/head.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React from 'react';
 
 export default function Head() {
   return (
     <>
       <title>Reserve at Milestones Grill (Toronto) | BookNow</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Restaurant booking page" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta
+        content="width=device-width, initial-scale=1"
+        name="viewport"
+      />
+      <meta
+        name="description"
+        content="Restaurant booking page"
+      />
+      <link
+        rel="icon"
+        href="/favicon.ico"
+      />
     </>
   );
 }
