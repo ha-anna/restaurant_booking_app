9314e11100c4c171cff51e98b2ad25d849116d45
Ha Anna <its.haanna@gmail.com>
2023-04-01 11:04:06 +0900

    Upload: displayed restaurant cards, dynamic price display

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
-import React from "react";
-import Header from "./components/Header";
-import RestaurantCard from "./components/RestaurantCard";
-import { PrismaClient } from "@prisma/client";
+import React from 'react';
+import Header from './components/Header';
+import RestaurantCard from './components/RestaurantCard';
+import { PrismaClient, Cuisine, Location, PRICE } from '@prisma/client';
+import { v4 as uuidv4 } from 'uuid';
 
 const prisma = new PrismaClient();
 
-const fetchRestaurants = async () => {
-  const restaurants = await prisma.restaurant.findMany();
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  slug: string;
+  location: Location;
+  price: PRICE;
+}
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      slug: true,
+      location: true,
+      price: true,
+    },
+  });
+
   return restaurants;
 };
 
@@ -17,9 +39,15 @@ export default async function Home() {
     <>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        {restaurants.map((restaurant) => (
-          <RestaurantCard />
-        ))}
+        {restaurants.map((restaurant) => {
+          const key = uuidv4();
+          return (
+            <RestaurantCard
+              key={key}
+              restaurant={restaurant}
+            />
+          );
+        })}
       </div>
     </>
   );
