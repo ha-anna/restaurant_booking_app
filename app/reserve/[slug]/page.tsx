461929a2d92227bfeb9c14e87d6272d949db9827
Ha Anna <its.haanna@gmail.com>
2023-04-11 21:56:16 +0900

    Fix: double navbar issue

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import NavBar from "../../components/NavBar";
-import Header from "./components/Header";
-import Form from "./components/Form";
+import React from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
 
 export default function ReserveRestaurant() {
   return (
     <main className="max-w-screen-2xl m-auto bg-white">
-      <NavBar />
       <div className="border-t h-screen flex justify-center">
         <div className="py-9 w-3/5 pl-4">
           <Header />
